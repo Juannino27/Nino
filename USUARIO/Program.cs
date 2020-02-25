b3798bf142225d3851b348d1e437a0bc40a92b19
Juannino27 <60374311+Juannino27@users.noreply.github.com>
2020-02-25 12:31:28 -0800

    Actividad 2/24/2020

---
+++ USUARIO/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+class CorreoElect
+{
+    private string Correo;
+    public CorreoElect(string Co) => Correo = Co;
+    public string getCorreo() => Correo;
+}
+class Usuario
+{
+    private string Nombre;
+    private List<CorreoElect> Correos;
+    public Usuario(string Nombre)
+    {
+        this.Nombre = Nombre;
+        Correos = new List<CorreoElect>();
+    }
+    public string getNombre() => Nombre;
+    public void AgregaaCorreo(CorreoElect Co)=> Correos.Add(Co);
+    public void Print() 
+    {
+        Console.WriteLine(Nombre);
+        foreach(CorreoElect Co in Correos)
+        {
+            Console.WriteLine(Co.getCorreo());
+        }
+    }
+    static void Main(string[] args)
+    {
+        Usuario U1 = new Usuario("Juan");
+        Console.WriteLine(U1.getNombre());
+        U1.AgregaaCorreo(new CorreoElect("juanito@gmail.com"));
+        U1.AgregaaCorreo(new CorreoElect("juanito@yahoo.com.mx"));
+        U1.Print();
+    }
+
+}
